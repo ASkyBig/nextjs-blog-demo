1ba950133e6ed1e57a97345aa420c925c54f9230
yangjian <yangjian@cambly.com>
2024-02-19 18:10:54 +0800

    feat(): mock api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,8 +71,49 @@ async function getFooterData() {
   };
 }
 
+async function getArticleData() {
+  return {
+    title: "Hello SSR!",
+    description: "A Demo for 《深入浅出SSR官网开发指南》",
+    list: [
+      {
+        label: "文章1",
+        info: "A test for article1",
+        link: "http://localhost:3000/article/1",
+      },
+      {
+        label: "文章2",
+        info: "A test for article2",
+        link: "http://localhost:3000/article/2",
+      },
+      {
+        label: "文章3",
+        info: "A test for article3",
+        link: "http://localhost:3000/article/3",
+      },
+      {
+        label: "文章4",
+        info: "A test for article4",
+        link: "http://localhost:3000/article/4",
+      },
+      {
+        label: "文章5",
+        info: "A test for article5",
+        link: "http://localhost:3000/article/5",
+      },
+      {
+        label: "文章6",
+        info: "A test for article6",
+        link: "http://localhost:3000/article/6",
+      },
+    ],
+  };
+}
+
 const RootLayout: FC<{ children: JSX.Element }> = async ({ children }) => {
   const { footerData } = await getFooterData();
+  // const articleData = await getArticleData();
+  // console.log("articleData ++++", articleData);
 
   return (
     <html lang="en">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { FC } from "react";
+import Link from "next/link";
+import getArticleData from "./api/home";
 
-const Home: FC = () => {
+const Home: FC = async () => {
+  const articleData = await getArticleData();
   return (
     <main className={styles.main}>
       <div className={styles.description}></div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.grid}>
+        {articleData?.list?.map((item, index) => (
+          <Link href={item.link} key={index}>
+            <div className={styles.card}>
+              <h2>{item.label} &rarr;</h2>
+              <p>{item.info}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </main>
   );
