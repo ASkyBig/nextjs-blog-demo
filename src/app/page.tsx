03bdd2046d56e19e9049deeb89efd9088a6bf9c6
jianyang <jianyang@cambly.com>
2024-02-26 13:36:23 +0800

    feat(): add datebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import styles from "./page.module.css";
 import { FC } from "react";
 import { getAllArticles, getArticlesByPageNo } from "@api/articles";
 import ArticleCom from "./components/article";
+import { sql } from "@vercel/postgres";
 
 const Home: FC = async () => {
   // by default, only need one interface
   const articles = await getArticlesByPageNo(1);
 
   const totalRes = await getAllArticles();
+
+  // const res = await sql`SELECT * FROM Pets;`;
+  // console.log("rows +++++", res);
 
   return <ArticleCom articles={articles} total={totalRes.length} />;
 };
