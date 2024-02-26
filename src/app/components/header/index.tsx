ebc584857ceadc4427580f91e84595109ebe3b3e
yangjian <yangj0412@foxmail.com>
2024-02-27 00:19:04 +0800

    fix(): ua

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -2,17 +2,34 @@
 import { FC, useContext } from "react";
 import styles from "./styles.module.scss";
 import { ThemeContext } from "@/stores/theme";
-import { Themes } from "@/constants/enum";
+import { UserAgentContext } from "@/stores/userAgent";
+import { Environment, Themes } from "@/constants/enum";
 import Links from "../navLink";
 
 export interface INavBarProps {}
+interface IData {
+  isMobile: boolean;
+}
 
-const Header: FC = () => {
+const Header = (data: IData) => {
   const { setTheme } = useContext(ThemeContext);
+  const { userAgent } = useContext(UserAgentContext);
 
   return (
     <div className={styles.header}>
       <Links />
+      {data.isMobile && <span className={styles.text}>mobile</span>}
+      {!data.isMobile && <span className={styles.text}>pc</span>}
+
+      {userAgent === Environment.pc && (
+        <span className={styles.text}>当前是pc端样式</span>
+      )}
+      {userAgent === Environment.ipad && (
+        <span className={styles.text}>当前是Ipad端样式</span>
+      )}
+      {userAgent === Environment.mobile && (
+        <span className={styles.text}>当前是移动端样式</span>
+      )}
       <div
         className={styles.themeIcon}
         onClick={(): void => {
--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,10 @@
 export const LOCAL_DOMAIN = "http://127.0.0.1:3000";
 export const CMS_DOMAIN = "http://127.0.0.1:1337";
+
+export const getIsMobile = (uaString: string | null) => {
+  console.log("uaString", uaString);
+
+  return /mobile|android|iphone|ipad|phone/i.test(
+    (uaString || "").toLowerCase()
+  );
+};
