d2eaa64d715d423703406e69481610232afde59b
yangjian <yangj0412@foxmail.com>
2024-02-28 23:39:43 +0800

    fix(): ts

--- src/app/i18n/index.ts
+++ src/app/i18n/index.ts
@@ -1,6 +1,6 @@
 // index.ts
 import { getOptions } from "./i18nConfig";
-import { InitOptions, createInstance, i18n } from "i18next";
+import { InitOptions, Resource, createInstance, i18n } from "i18next";
 import resourcesToBackend from "i18next-resources-to-backend";
 import { initReactI18next } from "react-i18next/initReactI18next";
 import { ITran } from "./type";
