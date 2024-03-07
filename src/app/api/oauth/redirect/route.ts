5d9c7f407eb90b0bd1f37068bd7842077cd15de1
jianyang <jianyang@cambly.com>
2024-03-07 18:02:00 +0800

    fix(): WIP

--- src/app/api/oauth/redirect/route.ts
+++ src/app/api/oauth/redirect/route.ts
@@ -3,11 +3,12 @@ import axios from "axios";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  console.log("searchParams", searchParams.get("code"));
   const requestToken = searchParams.get("code");
 
   const id = process.env.GITHUB_CLIENT_ID;
   const secret = process.env.GITHUB_SECRET;
+  console.log("tokenResponse", id, secret);
+
   try {
     const tokenResponse = await axios({
       method: "post",
@@ -23,21 +24,21 @@ export async function GET(request: Request) {
     const accessToken = tokenResponse.data.access_token;
     console.log("accessToken", accessToken);
 
-    const result = await axios({
-      method: "get",
-      url: `https://api.github.com/user`,
-      headers: {
-        accept: "application/json",
-        Authorization: `token ${accessToken}`,
-      },
-    });
-    console.log("result +++++", result);
+    // const result = await axios({
+    //   method: "get",
+    //   url: `https://api.github.com/user`,
+    //   headers: {
+    //     accept: "application/json",
+    //     Authorization: `token ${accessToken}`,
+    //   },
+    // });
+    // console.log("result +++++", result);
 
-    // const pageNo = (searchParams.get("pageNo") || 1) as number;
-    // return NextResponse.json({ name: result.data.name }, { status: 200 });
-    return NextResponse.redirect(
-      "http://localhost:3000?name=" + result.data.name
-    );
+    // // const pageNo = (searchParams.get("pageNo") || 1) as number;
+    // // return NextResponse.json({ name: result.data.name }, { status: 200 });
+    // return NextResponse.redirect(
+    //   "http://localhost:3000?name=" + result.data.name
+    // );
   } catch (error) {
     console.log("err ----", error);
     return NextResponse.json({ name: "test" }, { status: 200 });
